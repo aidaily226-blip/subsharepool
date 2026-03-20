db221a446c1ede4d1bb7c05dbea780f479a11801
aidaily226-blip <aidaily226@gmail.com>
2026-03-20 23:39:52 +0530

    fix output config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'], // Google profile pictures
+    domains: ['lh3.googleusercontent.com'],
   },
+  output: 'export',
 }
 
 module.exports = nextConfig
