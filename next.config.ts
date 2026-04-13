c7a6dc9f6573cfc066c7508b2089763d02384321
aidaily226-blip <aidaily226@gmail.com>
2026-04-13 19:33:36 +0530

    redirect www to non-www

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,21 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.subsharepool.com' }],
+        destination: 'https://subsharepool.com/:path*',
+        permanent: true,
+      },
+    ]
   },
 }
 
