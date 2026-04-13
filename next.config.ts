6c35ae63839be1dc6d1c141de6f7c02ac85e108b
aidaily226-blip <aidaily226@gmail.com>
2026-04-13 20:08:14 +0530

    revert config

--- next.config.ts
+++ next.config.ts
@@ -7,21 +7,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
     ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.subsharepool.com' }],
-        destination: 'https://subsharepool.com/:path*',
-        permanent: true,
-      },
-    ]
   },
 }
 
