eb04aafb364520cdebe8849a6369679bbb0eb139
aidaily226-blip <aidaily226@gmail.com>
2026-04-16 08:28:15 +0530

    remove www redirect from config

--- next.config.ts
+++ next.config.ts
@@ -13,16 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.subsharepool.com' }],
-        destination: 'https://subsharepool.com/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 export default nextConfig
